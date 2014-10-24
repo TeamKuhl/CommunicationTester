383698dd129bb5ace645cdab0e230f7595de94ac
Richard Schwab <mail@w.tf-w.tf>
2014-10-24 14:29:29 +0200

    Require --test argument for simple automated test Add interactive commandline client

--- Program.cs
+++ Program.cs
@@ -12,42 +12,95 @@ namespace CommunicationTester
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            int port = 45454;
+            if (args.Length == 1 && args[0] == "--test")
+            {
+                Server server = new Server();
+                int port = 45454;
 
-            log("Starting server on port " + port);
-            server.start(port);
+                log("Starting server on port " + port);
+                server.start(port);
 
-            log("Adding ServerReceiveHandler onReceive");
-            server.onReceive += new ServerReceiveHandler(
-                (TcpClient client, String type, String message) =>
-                {
-                    if (string.IsNullOrEmpty(type)) type = "(empty)";
-                    if (string.IsNullOrEmpty(message)) message = "(empty)";
-                    log("Server received: \"" + type + "\" | \"" + message + "\"");
-                    if(type == "PlayerInfo") {
-                        log("Server received PlayerInfo, answering with Join");
-                        server.sendToAll("Join", message);
+                log("Adding ServerReceiveHandler onReceive");
+                server.onReceive += new ServerReceiveHandler(
+                    (TcpClient client, String type, String message) =>
+                    {
+                        if (string.IsNullOrEmpty(type)) type = "(empty)";
+                        if (string.IsNullOrEmpty(message)) message = "(empty)";
+                        log("Server received: \"" + type + "\" | \"" + message + "\"");
+                        if (type == "PlayerInfo")
+                        {
+                            log("Server received PlayerInfo, answering with Join");
+                            server.sendToAll("Join", message);
+                        }
+                    }
+                );
+
+                Client c = new Client();
+
+                log("Adding ClientReceiveHandler onReceive");
+                c.onReceive += new ClientReceiveHandler(
+                    (string type, string message) =>
+                    {
+                        log("Client received: \"" + type + "\" | \"" + message + "\"");
+                    }
+                );
+
+                log("Client connects to the server");
+                bool connected = c.connect("127.0.0.1", port);
+                if (connected) log("Client connected to server");
+
+                log("Client sending PlayerInfo command with TestUser argument");
+                c.send("PlayerInfo", "TestUser");
+            }
+            else
+            {
+                // Interactive input
+
+                string defaultHost = "127.0.0.1";
+                int defaultPort = 45454;
+
+                Console.WriteLine("Starting interactive client...");
+                Console.Write("Please enter the hostname to connect to [" + defaultHost + "]: ");
+                string host = Console.ReadLine();
+                host = host.Trim();
+                if (String.IsNullOrEmpty(host)) host = defaultHost;
+                host = host.Trim();
+                log("Using host \"" + host + "\"");
+                Console.Write("Please enter the port you want to connect to [" + defaultPort + "]: ");
+                string sPort = Console.ReadLine();
+                int port;
+                if (String.IsNullOrEmpty(sPort)) port = defaultPort;
+                else {
+                    try
+                    {
+                        port = Convert.ToInt16(sPort);
+                    }
+                    catch (Exception)
+                    {
+                        port = 45454;
                     }
                 }
-            );
+                log("Using port \"" + port + "\"");
+                Client c = new Client();
+                Console.WriteLine("Adding ClientReceiveHandler onReceive");
+                c.onReceive += new ClientReceiveHandler(
+                    (string type, string message) =>
+                    {
+                        log("R: \"" + type + "\";\"" + message + "\"");
+                    }
+                );
 
-            Client c = new Client();
+                log("Connecting...");
+                c.connect(host, port);
 
-            log("Adding ClientReceiveHandler onReceive");
-            c.onReceive += new ClientReceiveHandler(
-                (string type, string message) =>
+                while (true)
                 {
-                    log("Client received: \"" + type + "\" | \"" + message + "\"");
+                    Console.Write("S: ");
+                    string line = Console.ReadLine();
+                    string[] data = line.Split(new Char[] { ';' }, 2);
+                    c.send(data[0], data[1]);
                 }
-            );
-
-            log("Client connects to the server");
-            bool connected = c.connect("127.0.0.1", port);
-            if (connected) log("Client connected to server");
-
-            log("Client sending PlayerInfo command with TestUser argument");
-            c.send("PlayerInfo", "TestUser");
+            }
         }
 
         private static void log(string message)
